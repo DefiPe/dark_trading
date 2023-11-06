0c36c32d24c94ff8616209a962ab0e5039b6d09c
Samrat Mukherjee <samratmukh1999@gmail.com>
2023-11-06 23:48:24 +0530

    header error solved

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
 import DexLayout from "@/layout/DexLayout/dexLayout";
 import { getdata } from "@/server/initialData";
+import Head from "next/head";
 //import { getServerSideProps } from "next/dist/build/templates/pages";
 
-const Dex = (props:any) => {
+const Dex = (props: any) => {
 	//console.log(props?.tokenJSON?.data);
 	return (
-		<main className="gray-dark text-foreground bg-background">
-			<DexLayout tokenData={props?.tokenJSON?.data}/>
-		</main>
-		
+		<>
+			<Head>
+				<title>DefiPe- Easy-to-use Crypto Trading Platform</title>
+				<meta
+					name="description"
+					content="Easy-to-use Crypto Trading Platform"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				{/* <link rel="icon" href="/Defipe_nav.ico" /> */}
+			</Head>
+			<main className="gray-dark text-foreground bg-background">
+				<DexLayout tokenData={props?.tokenJSON?.data} />
+			</main>
+		</>
+
+
 	);
 };
 
@@ -16,7 +29,7 @@ export async function getServerSideProps() {
 	//let tokenAPI = await fetch(`https://api.defipe.io/pagination/1?page=1&limit=10`);
 	let tokenJSON = await getdata();
 	return {
-		props: {tokenJSON}
+		props: { tokenJSON }
 	}
 }
 
