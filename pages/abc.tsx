cb4a1fbf8e2291af8cedbd5af831b2d1d33844b7
Samrat Mukherjee <samratmukh1999@gmail.com>
2023-11-08 21:49:35 +0530

    testing

--- pages/abc.tsx
+++ pages/abc.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       {/* <link rel="icon" href="/Defipe_nav.ico" /> */}
     </Head>
+    {console.log("hello")}
       <Button onPress={onOpen}>Open Modal</Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
